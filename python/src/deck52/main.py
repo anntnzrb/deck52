b66a41c577310269297883bb0d542c26a2b70628
anntnzrb <anntnzrb@protonmail.com>
2024-01-25 19:49:03 -0500

    refactor(python): use type annotations

--- python/src/deck52/main.py
+++ python/src/deck52/main.py
@@ -1,15 +1,16 @@
 import itertools as it
+from typing import Set, Tuple
 
 
-def genDeck():
-    ranks = set(str(n) for n in range(2, 11)).union({"J", "Q", "K", "A"})
-    suites = {"Clubs", "Diamonds", "Spades", "Hearts"}
+def genDeck() -> Set[Tuple[str, str]]:
+    ranks: Set[str] = {str(n) for n in range(2, 11)}.union({"J", "Q", "K", "A"})
+    suites: Set[str] = {"Clubs", "Diamonds", "Spades", "Hearts"}
 
     return set(it.product(ranks, suites))
 
 
-def main():
-    deck52 = genDeck()
+def main() -> None:
+    deck52: Set[Tuple[str, str]] = genDeck()
     print(deck52)
 
     assert len(deck52) == 52
